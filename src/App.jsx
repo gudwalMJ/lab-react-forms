01ec0a4284e7106f30313dedf0ad602c24b6ff0b
Josip Milan <josip.milan@aol.com>
2023-12-14 13:33:14 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,114 @@ import StudentCard from "./components/StudentCard";
 import studentsData from "./assets/students.json";
 
 function App() {
+  // Step 1: Create state variables
+  const [fullName, setFullName] = useState("");
+  const [image, setImage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [program, setProgram] = useState(""); // Set the default value based on the first option
+  const [graduationYear, setGraduationYear] = useState(2023); // Set a default value according to your use case
+  const [graduated, setGraduated] = useState(false); // Default value for checkbox
+
   const [students, setStudents] = useState(studentsData);
 
+  // Step 2: Create handler functions for each input
+  const handleFullNameChange = (e) => setFullName(e.target.value);
+  const handleImageChange = (e) => setImage(e.target.value);
+  const handlePhoneChange = (e) => setPhone(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleProgramChange = (e) => setProgram(e.target.value);
+  const handleGraduationYearChange = (e) => setGraduationYear(e.target.value);
+  const handleGraduatedChange = (e) => setGraduated(e.target.checked);
+
+  // Step 3: Create the form submission handler function
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newStudent = {
+      fullName,
+      image,
+      phone,
+      email,
+      program,
+      graduationYear,
+      graduated,
+    };
+
+    // Use the functional form of setStudents to ensure we have the latest state
+    setStudents((prevStudents) => [...prevStudents, newStudent]);
+
+    // Clear the form inputs
+    setFullName("");
+    setImage("");
+    setPhone("");
+    setEmail("");
+    setProgram("Web Dev");
+    setGraduationYear(2023);
+    setGraduated(false);
+  };
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={handleFullNameChange}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              value={image}
+              onChange={handleImageChange}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              value={phone}
+              onChange={handlePhoneChange}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select
+              name="program"
+              value={program}
+              onChange={handleProgramChange}
+            >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -56,6 +128,8 @@ function App() {
               name="graduationYear"
               type="number"
               placeholder="Graduation Year"
+              value={graduationYear}
+              onChange={handleGraduationYearChange}
               minLength={4}
               maxLength={4}
               min={2023}
@@ -65,19 +139,21 @@ function App() {
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              checked={graduated}
+              onChange={handleGraduatedChange}
+            />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
 
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
 
       {/* STUDENT LIST */}
       {students &&
